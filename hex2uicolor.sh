c3eaaf974dd4065605b5c21d1d12d3dde0d97abf
Arnold Sakhnov <egukin@gmail.com>
2013-01-04 21:52:41 -0800

    Create hex2uicolor.sh

---
+++ hex2uicolor.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+for f in "$@"
+do
+
+  # delete the preceding hash, if it’s there
+	value=${f#\#}
+
+	# extract the fragments of the original HEX code
+	red=`printf "%d" 0x${value:0:2}`
+	green=`printf "%d" 0x${value:2:2}`
+	blue=`printf "%d" 0x${value:4:2}`
+
+	# calculate new UIColor-compatible RGB values
+	redNum=`printf "%.3f" $(bc -l <<< "$red/255")`
+	greenNum=`printf "%.3f" $(bc -l <<< "$green/255")`
+	blueNum=`printf "%.3f" $(bc -l <<< "$blue/255")`
+
+	# put together the UIColor declaration
+	final=`echo "[UIColor colorWithRed:$redNum green:$greenNum blue:$blueNum alpha:1]"`
+
+	# final clean up (no trailing zeros)
+	echo $final | sed -E 's/\.?0*[ ]/ /g'
+
+done
